9d0a1809c1e9cb3e5790e2ea0b386668918af2bc
p3kfinger <dimapopov87@hotmail.com>
2022-06-20 07:30:34 +0100

    build commit

--- src/api.js
+++ src/api.js
@@ -44,5 +44,5 @@ export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}?key=${RE
 //Game ScreenShots
 export const gameScreenshotURL = (game_id) =>
   `${base_url}games/${game_id}/screenshots?key=${REACT_APP_API_KEY}`;
-//Searched game
+//Searched games
 export const searchGameURL = (game_name) => `${base_url}games?key=${REACT_APP_API_KEY}&search=${game_name}&page_size=10`;
